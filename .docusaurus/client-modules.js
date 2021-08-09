9a7bcaae631e2cea05a496fd3803d762602a24d4
valeriegrappendorf <37834853+valeriegrappendorf@users.noreply.github.com>
2021-08-09 14:40:43 +0200

    UI gh pages

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/Users/valeriegrappendorf/Desktop/iota-wiki/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/valeriegrappendorf/Desktop/iota-wiki/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/valeriegrappendorf/Desktop/iota-wiki/node_modules/@docusaurus/theme-classic/lib/admonitions.css"),
+  require("/Users/valeriegrappendorf/Desktop/iota-wiki/src/css/custom.css"),
+];
